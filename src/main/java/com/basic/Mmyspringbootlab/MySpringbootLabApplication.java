3ce8e1cec2dbf390fac487001a182be760ea564c
hhye0 <jhy030123@naver.com>
2025-05-07 22:53:12 +0900

    first commit

---
+++ src/main/java/com/basic/Mmyspringbootlab/MySpringbootLabApplication.java
@@ -0,0 +1,13 @@
+package com.basic.Mmyspringbootlab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MySpringbootLabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MySpringbootLabApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/basic/Mmyspringbootlab/MySpringbootLabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.basic.Mmyspringbootlab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MySpringbootLabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
